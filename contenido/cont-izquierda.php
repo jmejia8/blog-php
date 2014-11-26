9ad3cf8ade41cc41865b268091718baeefdb9164
jesus <jesusmejded@gmail.com>
2014-11-25 18:17:59 -0600

    Archivos de capitulo 16

--- contenido/cont-izquierda.php
+++ contenido/cont-izquierda.php
@@ -6,8 +6,14 @@
 <?
 $sql=mysql_query("SELECT * FROM post ORDER BY id DESC");
 $n = mysql_num_rows($sql);
-$pag = intval(quitar($_GET['pag']));
-$sql=mysql_query("SELECT * FROM post ORDER BY id DESC");
+if (isset($_GET['pag'])) {
+    $pag = intval(quitar($_GET['pag']))-1;
+}else{
+    $pag = 0;
+}
+$num_post =2;
+$show = $pag*$num_post;
+$sql=mysql_query("SELECT * FROM post ORDER BY id DESC LIMIT $show, $num_post ");
 while ($row=mysql_fetch_array($sql)) {
 
 ?>
@@ -47,12 +53,27 @@ while ($row=mysql_fetch_array($sql)) {
             <div class="clear"></div>
             <div class="pagination">
                 <ul>
-                    <li><a href="#">Anterior</a></li>
-                    <li><a href="#">1</a></li>
-                    <li><a href="#">2</a></li>
-                    <li><a href="#">3</a></li>
-                    <li><a href="#">4</a></li>
-                    <li><a href="#">Siguiente</a></li>
+                    <?
+                    if ($pag!=0) {
+                        $x=$pag;
+                       echo " <li><a href=\"index.php?pag=$x\">Anterior</a></li>";
+                    }else{
+                       echo " <li>Anterior</li>";
+                    }
+                    for ($i=1; $i < ($n/$num_post)+1; $i++) { 
+                        if ($pag==$i-1) {
+                            echo " <li class=\"pagination-activo\">$i</li>";
+                        }else{
+                            echo " <li><a href=\"index.php?pag=$i\">$i</a></li>";
+                        }
+                    }
+                    if ($pag!=$i-2) {
+                        $x=$pag+2;
+                        echo " <li><a href=\"index.php?pag=$x\">Siguiente</a></li>";
+                    }else{    
+                        echo " <li>Siguiente</li>";
+                    }
+                    ?>
                 </ul>
             </div>
           <div class="clear"></div>
